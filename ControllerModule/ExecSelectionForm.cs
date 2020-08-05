0c25f48327ab273c985bef57bb8234d3ce991a38
Micsvir <62797485+Micsvir@users.noreply.github.com>
2020-08-05 22:49:43 +0300

    Update ExecSelectionForm.cs

--- ControllerModule/ExecSelectionForm.cs
+++ ControllerModule/ExecSelectionForm.cs
@@ -20,27 +20,7 @@ namespace ControllerModule
 
         private void cbExecutorsList_Click(object sender, EventArgs e)
         {
-            /*
-            cbExecutorsList.Items.Clear();
-            try
-            {
-                //Запрос списка групп
-                NetMessage getGroupsListMsg = new NetMessage(NetMessage.commandType.GetInfo, "GetGroups");
-                MainForm.client.SendMessage(getGroupsListMsg, false);
 
-                //заполнение cbExecutorsList
-                for (int row = 0; row < MainForm.receivedData.Rows.Count; row++)
-                {
-                    cbExecutorsList.Items.Add(MainForm.receivedData.Rows[row]["GroupName"]);
-                }
-
-                MainForm.groupsData = MainForm.receivedData;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            */
         }
 
         private void cbExecutorsList_SelectedIndexChanged(object sender, EventArgs e)
